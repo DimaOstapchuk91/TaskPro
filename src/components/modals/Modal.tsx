8d09f9c17017c28965da3b090a652901c2f8f4c8
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-09-20 12:17:50 +0300

    add Modal wraper

---
+++ src/components/modals/Modal.tsx
@@ -0,0 +1,64 @@
+import { useEffect } from 'react';
+import sprite from '../../assets/sprite.svg';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.getElementById('modal-root') as HTMLElement;
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+      document.body.classList.add('noScroll');
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.body.classList.remove('noScroll');
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div
+      className='fixed top-0 left-0 w-screen h-screen bg-text-gray flex justify-center items-center z-50'
+      onClick={onClose}
+    >
+      <div
+        className='relative w-max overflow-hidden'
+        onClick={e => e.stopPropagation()}
+      >
+        <button
+          className='absolute group top-5 right-5 cursor-pointer hover'
+          onClick={onClose}
+        >
+          <svg
+            className='stroke-text-dark group-hover:stroke-hover transition-all duration-200'
+            width={24}
+            height={24}
+          >
+            <use href={`${sprite}#icon-close`} />
+          </svg>
+        </button>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default Modal;
